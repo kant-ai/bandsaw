5c55d87b2a2b130b8f5b504d35863df671241623
Christoph Emonds <chris+git@kant.ai>
2021-11-07 16:12:59 +0100

    Moved test types into test functions
    This commit moves some dummy types into the test where the types are
    actually used to clean up the test module and improve the readability
    of the tests.

--- test/test_session.py
+++ test/test_session.py
@@ -1,6 +1,7 @@
 import io
 import threading
 import unittest
+import unittest.mock
 
 from bandsaw.advice import Advice
 from bandsaw.config import Configuration
@@ -74,102 +75,110 @@ class MyConcurrentTask:
         return threading.current_thread().ident
 
 
-class NoProceedingAdvice(Advice):
-
-    def before(self, session):
-        pass
-
-
-class DoubleProceedingAdvice(Advice):
-
-    def before(self, session):
-        session.proceed()
-        session.proceed()
-
-
-class MyExtension(Extension):
-    def __init__(self):
-        self.init_called = False
-        self.before_called = False
-        self.after_called = False
-
-    def on_init(self, configuration):
-        self.init_called = True
-
-    def on_before_advice(self, task, run, context):
-        self.before_called = True
-
-    def on_after_advice(self, task, run, context, result):
-        self.after_called = True
-
-
-extension = MyExtension()
-
-
-configuration = Configuration()
-configuration.add_advice_chain(MySavingAdvice(), name='save')
-configuration.add_advice_chain(MyConcurrentAdvice(), name='concurrent')
-configuration.add_advice_chain(NoProceedingAdvice(), name='no-proceeding')
-configuration.add_advice_chain(DoubleProceedingAdvice(), name='double-proceeding')
-configuration.add_extension(extension)
-
-
 class TestSession(unittest.TestCase):
 
+    def setUp(self):
+        self.config = Configuration()
+        self.config.add_advice_chain(MySavingAdvice(), name='save')
+
     def test_empty_advice_returns_execution_result(self):
-        session = Session(MyTask(), Run('1'), configuration)
+        session = Session(MyTask(), Run('1'), self.config)
         result = session.initiate()
         self.assertTrue(result)
 
     def test_extensions_are_called(self):
-        global extension
-        session = Session(MyTask(), Run('1'), configuration)
+        class MyExtension(Extension):
+            def __init__(self):
+                self.init_called = False
+                self.before_called = False
+                self.after_called = False
+
+            def on_init(self, configuration):
+                self.init_called = True
+
+            def on_before_advice(self, task, run, context):
+                self.before_called = True
+
+            def on_after_advice(self, task, run, context, result):
+                self.after_called = True
+
+        extension = MyExtension()
+        self.config.add_extension(extension)
+        session = Session(MyTask(), Run('1'), self.config)
         session.initiate()
         self.assertTrue(extension.before_called)
         self.assertTrue(extension.after_called)
 
     def test_no_proceeding_advice_raises_an_error(self):
+        class NoProceedingAdvice(Advice):
+
+            def before(self, session):
+                pass
+
+        self.config.add_advice_chain(NoProceedingAdvice(), name='no-proceeding')
+
         with self.assertRaisesRegex(RuntimeError, 'Not all advice.*NoProceedingAdvice'):
-            session = Session(MyTask(), Run('1'), configuration, 'no-proceeding')
+            session = Session(MyTask(), Run('1'), self.config, 'no-proceeding')
             session.initiate()
 
     def test_double_proceeding_advice_raises_an_error(self):
+        class DoubleProceedingAdvice(Advice):
+
+            def before(self, session):
+                session.proceed()
+                session.proceed()
+
+        self.config.add_advice_chain(DoubleProceedingAdvice(), name='double-proceeding')
+
         with self.assertRaisesRegex(RuntimeError, 'Session already finished'):
-            session = Session(MyTask(), Run('1'), configuration, 'double-proceeding')
+            session = Session(MyTask(), Run('1'), self.config, 'double-proceeding')
             session.initiate()
 
     def test_advice_can_save_and_resume_session(self):
-        session = Session(MyTask(), Run('1'), configuration, 'save')
-        result = session.initiate()
-        self.assertTrue(result)
+        with unittest.mock.patch("bandsaw.session.get_configuration", return_value=self.config):
+            session = Session(MyTask(), Run('1'), self.config, 'save')
+            result = session.initiate()
+            self.assertTrue(result)
 
     def test_session_restore_updates_configuration(self):
-        session = Session(MyTask(), Run('1'), configuration)
+        with unittest.mock.patch("bandsaw.session.get_configuration", return_value=self.config):
+            session = Session(MyTask(), Run('1'), self.config)
 
-        stream = io.BytesIO()
-        session.save(stream)
-        stream.seek(0)
+            stream = io.BytesIO()
+            session.save(stream)
+            stream.seek(0)
 
-        restored_session = Session().restore(stream)
+            restored_session = Session().restore(stream)
 
-        self.assertEqual(
-            session._configuration.module_name,
-            restored_session._configuration.module_name,
-        )
-        self.assertEqual(session._advice_chain, restored_session._advice_chain)
-        self.assertEqual(session.context, restored_session.context)
+            self.assertEqual(
+                session._configuration.module_name,
+                restored_session._configuration.module_name,
+            )
+            self.assertEqual(session._advice_chain, restored_session._advice_chain)
+            self.assertEqual(session.context, restored_session.context)
 
     def test_session_run_parts_in_new_thread(self):
-        session = Session(MyConcurrentTask(), Run('1'), configuration, 'concurrent')
+        self.config.add_advice_chain(MyConcurrentAdvice(), name='concurrent')
 
-        result = session.initiate()
-        self.assertNotEqual(threading.current_thread().ident, result)
+        with unittest.mock.patch("bandsaw.session.get_configuration", return_value=self.config):
+            session = Session(MyConcurrentTask(), Run('1'), self.config, 'concurrent')
+
+            result = session.initiate()
+            self.assertNotEqual(threading.current_thread().ident, result)
 
     def test_session_uses_serializer_from_configuration(self):
-        session = Session(MyConcurrentTask(), Run('1'), configuration, 'concurrent')
+        session = Session(MyTask(), Run('1'), self.config)
 
         serializer = session.serializer
-        self.assertIs(serializer, configuration.serializer)
+        self.assertIs(serializer, self.config.serializer)
+
+
+class MyAdvice1(Advice):
+    pass
+
+
+class MyAdvice2(Advice):
+    pass
 
 
 class TestModerator(unittest.TestCase):
@@ -188,33 +197,33 @@ class TestModerator(unittest.TestCase):
         self.assertEqual(queue.task_called, deserialized.task_called)
 
     def test_current_advice_is_before(self):
-        queue = _Moderator([NoProceedingAdvice(), MySavingAdvice()])
+        queue = _Moderator([MyAdvice1(), MyAdvice2()])
         queue.before_called = 1
         queue.after_called = 0
         queue.task_called = False
 
-        self.assertIsInstance(queue.current_advice, NoProceedingAdvice)
+        self.assertIsInstance(queue.current_advice, MyAdvice1)
 
         queue.before_called = 2
-        self.assertIsInstance(queue.current_advice, MySavingAdvice)
+        self.assertIsInstance(queue.current_advice, MyAdvice2)
 
     def test_current_advice_is_after(self):
-        queue = _Moderator([NoProceedingAdvice(), MySavingAdvice()])
+        queue = _Moderator([MyAdvice1(), MyAdvice2()])
         queue.before_called = 2
         queue.after_called = 1
         queue.task_called = True
 
-        self.assertIsInstance(queue.current_advice, MySavingAdvice)
+        self.assertIsInstance(queue.current_advice, MyAdvice2)
 
         queue.after_called = 2
-        self.assertIsInstance(queue.current_advice, NoProceedingAdvice)
+        self.assertIsInstance(queue.current_advice, MyAdvice1)
 
     def test_current_advice_is_None_without_advices(self):
         queue = _Moderator()
         self.assertIsNone(queue.current_advice)
 
     def test_current_advice_is_None_when_finished(self):
-        queue = _Moderator([NoProceedingAdvice()])
+        queue = _Moderator([MyAdvice1()])
         queue.before_called = 1
         queue.after_called = 1
         queue.task_called = True
