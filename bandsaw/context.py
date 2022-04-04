8e1055cd95ce6021a67dd7b2ab84bf3c3f5adb5f
Christoph Emonds <chris+git@kant.ai>
2022-04-04 19:15:28 +0200

    Implement mapping in Context type

--- bandsaw/context.py
+++ bandsaw/context.py
@@ -1,15 +1,26 @@
 """Classes that represent the context used in advising tasks."""
+import collections.abc
+
 from .serialization import SerializableValue
 
 
-class Context(SerializableValue):
+class Context(collections.abc.MutableMapping, SerializableValue):
     """
     Class for representing the context for advising tasks.
 
     The context contains of a set of arbitrary key-value mappings that can be used
     by the `Advice` classes to store state or communicate with other advices.
 
+    As a default, non-existing properties return a `Context` themselves, which allows
+    setting values within a hierarchy, without the need to check for the existence of
+    parent contexts:
 
+    Examples:
+        >>> context = Context()
+        >>> context['a']['b'] = 1
+
+    Note:
+        If a value has been set once, it can't be overwritten or removed!
     """
 
     def __init__(self, attributes=None):
@@ -33,6 +44,29 @@ class Context(SerializableValue):
         `Advice` can add to this mapping and use this as a way of keeping state.
         """
         return self._attributes
+
+    def __getitem__(self, key):
+        if key in self._attributes:
+            return self._attributes[key]
+        else:
+            child_context = Context()
+            self._attributes[key] = child_context
+            return child_context
+
+    def __setitem__(self, key, item):
+        if key in self._attributes:
+            raise TypeError("Overwriting values in Context is not supported.")
+
+        self._attributes[key] = item
+
+    def __iter__(self):
+        return iter(self.attributes)
+
+    def __delitem__(self, key):
+        raise TypeError("Deleting values from Context is not supported.")
+
+    def __len__(self):
+        return len(self._attributes)
 
     def __eq__(self, other):
         if not isinstance(other, type(self)):
--- test/test_context.py
+++ test/test_context.py
@@ -5,9 +5,6 @@ from bandsaw.context import Context
 
 class TestContext(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_contexts_with_same_task_arguments_are_equal(self):
         context1 = Context({'a': 1})
         context2 = Context({'a': 1})
@@ -37,6 +34,58 @@ class TestContext(unittest.TestCase):
         context2 = {'a': 1}
 
         self.assertNotEqual(context1, context2)
+
+    def test_contexts_can_be_used_as_dicts_when_setting_properties(self):
+        context = Context()
+        context['my-value'] = 1
+
+        self.assertEqual(context.attributes['my-value'], 1)
+
+    def test_contexts_can_be_used_as_dicts_when_getting_properties(self):
+        context = Context({'my-value': 1})
+
+        self.assertEqual(context['my-value'], 1)
+
+    def test_contexts_can_iterate_over_properties(self):
+        context = Context({'my-value': 1, 'my-other': 2})
+
+        keys = []
+        for key in context:
+            keys.append(key)
+
+        self.assertEqual(keys, ['my-value', 'my-other'])
+
+    def test_contexts_cant_delete_properties(self):
+        context = Context({'my-value': 1})
+
+        with self.assertRaisesRegex(TypeError, "Deleting values from Context is not supported."):
+            del context['my-value']
+
+    def test_contexts_cant_overwrite_properties(self):
+        context = Context({'my-value': 1})
+
+        with self.assertRaisesRegex(TypeError, "Overwriting values in Context is not supported."):
+            context['my-value'] = 2
+
+    def test_contexts_support_len(self):
+        context = Context({'my-value': 1, 'my-other': 2})
+        self.assertEqual(2, len(context))
+
+    def test_contexts_support_len(self):
+        context = Context({'my-value': 1, 'my-other': 2})
+        self.assertEqual(2, len(context))
+
+    def test_contexts_create_empty_contexts_as_default_value_for_parents(self):
+        context = Context()
+        context['parent']['value'] = 1
+
+        self.assertEqual(context.attributes['parent']['value'], 1)
+
+    def test_contexts_create_empty_contexts_as_default_value_for_parents(self):
+        context = Context()
+        context['parent']['value'] = 1
+
+        self.assertEqual(context.attributes['parent']['value'], 1)
 
 
 if __name__ == '__main__':
