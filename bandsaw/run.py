7884fbe22dfdd2be66530b6beaddc8639dc524b4
Christoph Emonds <chris+git@kant.ai>
2022-05-06 22:12:28 +0200

    Add additional information about runs of a workflow
    This commit adds a new class Run which keeps information about the
    environment in which a workflow is being run.

--- bandsaw/run.py
+++ bandsaw/run.py
@@ -1,12 +1,97 @@
 """Functions for managing the run id."""
 import logging
+import os
+import sys
 import uuid
+
+from bandsaw.datetime import current_timestamp
+from bandsaw.user import get_current_username
 
 
 logger = logging.getLogger(__name__)
 
 
 _RUN_ID = None
+_RUN = None
+
+
+class Run:
+    """
+    A Run represents a single run of a workflow.
+
+    It contains additional meta data about the run, like e.g. the timestamp the run was
+    started or other useful data. Once set, the run doesn't change.
+    """
+
+    #  pylint: disable=too-many-arguments
+
+    def __init__(
+        self,
+        run_id,
+        start_date=None,
+        user=None,
+        command=None,
+        env=None,
+        meta=None,
+    ):
+        self.run_id = run_id
+        self.start_date = start_date or current_timestamp()
+        self.user = user or get_current_username()
+        self.command = command or sys.argv
+        self.env = env or dict(os.environ)
+        self.meta = meta or {}
+
+    def to_json(self):
+        """
+        Returns a JSON representation for serializing the run.
+
+        Returns:
+            dict: A dictionary with the JSON values.
+        """
+        return {
+            'id': self.run_id,
+            'start_date': self.start_date,
+            'user': self.user,
+            'command': self.command,
+            'env': self.env,
+            'meta': self.meta,
+        }
+
+    @classmethod
+    def from_json(cls, run_json):
+        """
+        Recreate a Run instance from its JSON representation.
+
+        Args:
+            run_json:
+
+        Returns:
+            Run: The recreated Run instance.
+        """
+        return Run(
+            run_json['id'],
+            start_date=run_json['start_date'],
+            user=run_json['user'],
+            command=run_json['command'],
+            env=run_json['env'],
+            meta=run_json['meta'],
+        )
+
+
+def get_run():
+    """
+    Returns the run.
+
+    The run contains information about the environment of a specific
+    run of a workflow.
+
+    Returns:
+        bandsaw.run.Run: The single run.
+    """
+    global _RUN  # pylint: disable=global-statement
+    if _RUN is None:
+        _RUN = Run(get_run_id())
+    return _RUN
 
 
 def get_run_id():
--- test/test_run.py
+++ test/test_run.py
@@ -1,6 +1,75 @@
+import json
+import os
+import sys
 import unittest
 
 import bandsaw.run
+from bandsaw.user import get_current_username
+
+
+class TestRun(unittest.TestCase):
+
+    def test_new_run_contains_start_date(self):
+        run = bandsaw.run.Run('my-run-id')
+        self.assertIsInstance(run.start_date, str)
+        self.assertRegex(run.start_date, r'^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{3}[+-]\d\d:\d\d$')
+
+    def test_new_run_contains_current_user(self):
+        run = bandsaw.run.Run('my-run-id')
+        self.assertEqual(run.user, get_current_username())
+
+    def test_new_run_contains_command(self):
+        run = bandsaw.run.Run('my-run-id')
+        self.assertEqual(run.command, sys.argv)
+
+    def test_new_run_contains_environment(self):
+        run = bandsaw.run.Run('my-run-id')
+        self.assertEqual(run.env, os.environ)
+
+    def test_new_run_contains_meta_data(self):
+        run = bandsaw.run.Run('my-run-id', meta={'my': 'meta'})
+        self.assertEqual(run.meta, {'my': 'meta'})
+
+    def test_run_can_be_written_as_json(self):
+        run = bandsaw.run.Run(
+            'my-run-id',
+            start_date='my-date',
+            user='my-user',
+            command='/my-command',
+            env={'env': 'variable'},
+            meta={'my': 'meta'},
+        )
+        json_string = json.dumps(run.to_json())
+        self.assertEqual(json_string, '{"id": "my-run-id", "start_date": "my-date", "user": "my-user", "command": "/my-command", "env": {"env": "variable"}, "meta": {"my": "meta"}}')
+
+    def test_run_can_be_recreated_from_json(self):
+        run = bandsaw.run.Run(
+            'my-run-id',
+            start_date='my-date',
+            user='my-user',
+            command='/my-command',
+            env={'env': 'variable'},
+            meta={'my': 'meta'},
+        )
+        new_run = bandsaw.run.Run.from_json(run.to_json())
+        self.assertEqual(run.run_id, new_run.run_id)
+        self.assertEqual(run.start_date, new_run.start_date)
+        self.assertEqual(run.user, new_run.user)
+        self.assertEqual(run.command, new_run.command)
+        self.assertEqual(run.env, new_run.env)
+        self.assertEqual(run.meta, new_run.meta)
+
+
+class TestGetRun(unittest.TestCase):
+
+    def test_get_run_returns_a_run_instance(self):
+        run = bandsaw.run.get_run()
+        self.assertIsInstance(run, bandsaw.run.Run)
+
+    def test_get_run_returns_the_same_instance_in_multiple_calls(self):
+        run1 = bandsaw.run.get_run()
+        run2 = bandsaw.run.get_run()
+        self.assertIs(run1, run2)
 
 
 class TestRunId(unittest.TestCase):
